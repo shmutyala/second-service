184522376cc70eac4476edb034bbde8d1e10489c
shmutyala <shmutyala@USHYDSHMUTYALA2.us.deloitte.com>
2020-01-19 18:02:22 +0530

    Implemented Second Service

---
+++ src/main/java/com/shrikantmutyala/controller/SecondController.java
@@ -0,0 +1,15 @@
+package com.shrikantmutyala.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/consumer")
+public class SecondController {
+
+	@GetMapping("/message")
+	public String test(@RequestHeader("second-request") String header) {
+		System.out.println(header);
+		return "Hello JavaInUse Called in second Service";
+	}
+
+}
